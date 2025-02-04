fe87dd87edaf65a3c97ece8bccc250054d0b98cd
forbade <truisms@proton.me>
2025-02-04 22:19:53 +0800

    feat: add system-level Brave config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -16,13 +16,10 @@
   };
 
   # Program Installation
+  # NOTE: program configurations are stored inside of ../modules 
   programs.brave.enable = true;
   programs.vscode.enable = true;
-
-  programs.chromium = {
-	enable = true;
-	package = pkgs.brave;
-  };
+  programs.spotify-player.enable = true;
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
