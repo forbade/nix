e087d0d46e1e4d6a8c74aa62719e0211d511e0cd
forbade <truisms@proton.me>
2025-02-12 14:29:52 +0800

    fix: attempt to fix babel-loading from init.el

--- home-manager/emacs/init.el
+++ home-manager/emacs/init.el
@@ -1,7 +1,4 @@
-(org-babel-load-file
-   (expand-file-name
-       "config.org"
-         user-emacs-directory))
+(org-babel-load-file "config.org")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
